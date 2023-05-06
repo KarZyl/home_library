de21e2b51b7aed18626bfce9daeedbecf14506e3
Karol Żyłka <zylkakarol@gmail.com>
2023-05-06 14:51:33 +0200

    Upgrade to REST API

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from flask import Flask, render_template, request
 import sqlite3
+from werkzeug.exceptions import BadRequestKeyError
 
 
 conn = sqlite3.connect('domowa_biblioteka.db')
@@ -30,85 +32,96 @@ c.execute('''CREATE TABLE IF NOT EXISTS wypozyczenia
              FOREIGN KEY (nazwisko_autora) REFERENCES autorzy(nazwisko),
              FOREIGN KEY (tytul) REFERENCES ksiazki(tytul))''')
 
-
-# dane testowe- usunąć po pierwszym uruchomieniu aplikacji
+"""
+# dane testowe- do pierwszego uruchomienia aplikacji
 c.execute("INSERT INTO autorzy (imie, nazwisko) VALUES (?, ?)", ('Adam', 'Mickiewicz'))
 c.execute("INSERT INTO ksiazki (tytul, rok_wydania, imie_autora, nazwisko_autora, status) VALUES (?, ?, ?, ?, ?)", ('Pan Tadeusz', 1834, 'Adam', 'Mickiewicz', 'niedostępna'))
 c.execute("INSERT INTO wypozyczenia (tytul, nazwisko_autora, data_wypozyczenia, data_zwrotu) VALUES (?, ?, ?, ?)", ('Pan Tadeusz', 'Adam', '2022-06-01', '2022-06-15'))
-
-
+"""
 conn.commit()
 
-# menu główne
-while True:
-    print('''
-        Witaj w Domowej Bibliotece!
 
-        Wybierz jedną z opcji:
-        1. Dodaj autora
-        2. Dodaj książkę
-        3. Wypożycz książkę
-        4. Oddaj książkę
-        5. Wyświetl listę autorów
-        6. Wyświetl listę książek
-        7. Wyświetl listę wypożyczeń
-        8. Wyjdź z programu
-    ''')
-
+app = Flask(__name__)
     
-    wybor = input("Twój wybór: ")
+@app.route('/')
+def homepage():
+    return render_template('index.html')
 
-    # dodanie autora
-    if wybor == '1':
-        imie = input("Podaj imię autora: ")
-        nazwisko = input("Podaj nazwisko autora: ")
+@app.route('/dodaj_autora', methods=['GET', 'POST'])
+def dodaj_autora():
+    if request.method == 'POST':
+        imie = request.form['imie']
+        nazwisko = request.form['nazwisko']
+        conn = sqlite3.connect('domowa_biblioteka.db')
+        c = conn.cursor()
         c.execute("INSERT INTO autorzy (imie, nazwisko) VALUES (?, ?)", (imie, nazwisko))
         conn.commit()
-        print("Dodano autora!")
+        conn.close()
+        komunikat = f"Dodano autora  {imie} {nazwisko}"
+        return render_template('dodaj_autora.html', komunikat=komunikat)
+    else:
+        return render_template('dodaj_autora.html')
 
-    # dodanie książki
-    elif wybor == '2':
-        tytul = input("Podaj tytuł książki: ")
-        rok_wydania = input("Podaj rok wydania książki: ")
-        imie_autora = input("Podaj imię autora książki: ")
-        nazwisko_autora = input("Podaj nazwisko autora książki: ")
 
+@app.route('/dodaj_ksiazke', methods=['GET','POST'])
+def dodaj_ksiazke():
+    if request.method == 'POST':
+        tytul = request.form['tytul']
+        rok_wydania = request.form['rok_wydania']
+        imie_autora = request.form['imie_autora']
+        nazwisko_autora = request.form['nazwisko_autora']
+        conn = sqlite3.connect('domowa_biblioteka.db')
+        c = conn.cursor()
         c.execute("SELECT id FROM autorzy WHERE imie = ? AND nazwisko = ?", (imie_autora, nazwisko_autora))
         autor = c.fetchone()
-
         if autor is None:
             c.execute("INSERT INTO autorzy (imie, nazwisko) VALUES (?, ?)", (imie_autora, nazwisko_autora))
             conn.commit()
             autor_id = c.lastrowid
-            print("Dodano nowego autora!")
+            komunikat_A= f"Dodano nowego autora - {imie_autora} {nazwisko_autora}"
         else:
-            autor_id = autor[0]      
+            autor_id = autor[0]  
+            komunikat_A= f"Autor - {imie_autora} {nazwisko_autora} znajduje się już w bazie danych"    
         c.execute("INSERT INTO ksiazki (tytul, rok_wydania, imie_autora, nazwisko_autora, status) VALUES (?, ?, ?, ?, ?)", (tytul, rok_wydania, imie_autora, nazwisko_autora, 'dostępna'))
         conn.commit()
-        print("Dodano książkę!")
+        komunikat=f"Dodano książkę {tytul}"
+        conn.close()
+        return render_template('dodaj_ksiazke.html', komunikat=komunikat, komunikat_A=komunikat_A)
+    else:
+        return render_template('dodaj_ksiazke.html')
         
-    # wypożyczenie książki
-    elif wybor == '3':
-        tytul = input("Podaj tytuł książki do wypożyczenia: ")
+@app.route('/wypozycz_ksiazke', methods=['GET', 'POST'])
+def wypozycz_ksiazke():
+    if request.method == 'POST':
+        tytul = request.form['tytul']
+        conn = sqlite3.connect('domowa_biblioteka.db')
+        c = conn.cursor()
         c.execute("SELECT * FROM ksiazki WHERE tytul=?", (tytul,))
         ksiazka = c.fetchone()
         if ksiazka:
             if ksiazka[5] == 'dostępna':
                 c.execute("UPDATE ksiazki SET status='niedostępna' WHERE tytul=?", (tytul,))
                 nazwisko_autora = ksiazka[4]
-                data_wypozyczenia = input("Podaj datę wypożyczenia książki w formacie RRRR-MM-DD: ")
-                data_zwrotu = input("Podaj datę zwrotu książki w formacie RRRR-MM-DD: ")
+                data_wypozyczenia = request.form['data_wypozyczenia']
+                data_zwrotu = request.form['data_zwrotu']
                 c.execute("INSERT INTO wypozyczenia (tytul, nazwisko_autora, data_wypozyczenia, data_zwrotu) VALUES (?, ?, ?, ?)", (tytul, nazwisko_autora, data_wypozyczenia, data_zwrotu))
                 conn.commit()
-                print("Książka została wypożyczona!")
+                komunikat= f"Książka {tytul} została wypożyczona!"
             else:
-                print("Książka jest już wypożyczona.")
+                komunikat= f"Książka {tytul} jest już wypożyczona."
         else:
-            print("Nie znaleziono książki o podanym tytule.")
+            komunikat= f"Nie znaleziono książki {tytul}"
+        conn.close()
+        return render_template('wypozycz_ksiazke.html', komunikat=komunikat)
+    else:
+        return render_template('wypozycz_ksiazke.html')
 
-    # oddanie książki
-    elif wybor == '4':
-        tytul = input("Podaj tytuł książki do oddania: ")
+@app.route('/oddaj_ksiazke', methods=['GET','POST'])
+def oddaj_ksiazke():
+    if request.method == 'POST':
+        tytul = request.form['tytul']
+        conn = sqlite3.connect('domowa_biblioteka.db')
+        c = conn.cursor()
         c.execute("SELECT * FROM ksiazki WHERE tytul=?", (tytul,))
         ksiazka = c.fetchone()
         if ksiazka:
@@ -118,41 +131,43 @@ while True:
             if wypozyczenie:
                 c.execute("DELETE FROM wypozyczenia WHERE id=?", (wypozyczenie[0],))
                 conn.commit()
-                print("Książka została oddana!")
+                komunikat =f"Książka {tytul} została oddana!"
             else:
-                print("Nie znaleziono wypożyczenia dla podanego tytułu.")
+                komunikat=f"Nie znaleziono wypożyczenia {tytul}"
         else:
-            print("Nie znaleziono książki o podanym tytule.")
-
-    # wyświetlenie listy autorów
-    elif wybor == '5':
-        c.execute("SELECT * FROM autorzy")
-        autorzy = c.fetchall()
-        print("Lista autorów:")
-        for autor in autorzy:
-            print("- {} {}".format(autor[1], autor[2]))
-
-    # wyświetlenie listy książek
-    elif wybor == '6':
-        c.execute("SELECT * FROM ksiazki")
-        ksiazki = c.fetchall()
-        print("Lista książek:")
-        for ksiazka in ksiazki:
-            print("- {} ({}) - {}, {}, status: {}".format(ksiazka[1], ksiazka[2], ksiazka[3], ksiazka[4], ksiazka[5]))
-
-    # wyświetlenie listy wypożyczeń
-    elif wybor == '7':
-        c.execute("SELECT * FROM wypozyczenia")
-        wypozyczenia = c.fetchall()
-        print("Lista wypożyczeń:")
-        for wypozyczenie in wypozyczenia:
-            print("- {} - od: {}, do: {}".format(wypozyczenie[0], wypozyczenie[3], wypozyczenie[4]))
-
-    # wyjście z programu
-    elif wybor == '8':
-        print("Dziękujemy za korzystanie z Domowej Biblioteki!")
-        break
-
-    # nieznany wybór
+            komunikat=f"Nie znaleziono książki {tytul}"
+        conn.close()
+        return render_template('oddaj_ksiazke.html', komunikat=komunikat)
     else:
-        print("Nieznana opcja. Wybierz ponownie.")
+        return render_template('oddaj_ksiazke.html')
+    
+@app.route('/autorzy', methods=['GET'])
+def autorzy():
+    conn = sqlite3.connect('domowa_biblioteka.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM autorzy")
+    autorzy = c.fetchall()
+    conn.close()
+    return render_template('autorzy.html', autorzy=autorzy)
+
+@app.route('/wypozyczenia', methods=['GET'])
+def wypozyczenia():
+    conn = sqlite3.connect('domowa_biblioteka.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM wypozyczenia")
+    wypozyczenia = c.fetchall()
+    conn.close()
+    return render_template('wypozyczenia.html', wypozyczenia=wypozyczenia)
+
+@app.route('/ksiazki', methods=['GET'])
+def ksiazki():
+    conn = sqlite3.connect('domowa_biblioteka.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM ksiazki")
+    ksiazki = c.fetchall()
+    conn.close()
+    return render_template('ksiazki.html', ksiazki=ksiazki)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
